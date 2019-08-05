4c2da995877c89e65b0b5d664dd4e1a32780b372
Travis CI User <undefined>
2019-08-05 18:18:23 +0000

    Deployed to Github Pages

---
+++ generate-typescript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# TODO: fix
+../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator generate -g typescript-fetch -i swagger.yml -o typescript-fetch-es6 -c config.json
+cp ./typescript-fetch-transpile/* typescript-fetch-es6
+cd typescript-fetch-es6
+npm install
+npm run build
