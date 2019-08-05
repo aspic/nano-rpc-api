1064b46ca3c90c613b1580723c89802f77dc7c3b
Kjetil Mehl <kjetil@mehl.no>
2019-08-05 23:42:14 +0200

    Moves files

--- generate-typescript.sh
+++ generate-typescript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TODO: fix
-../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator generate -g typescript-fetch -i swagger.yml -o typescript-fetch-es6 -c config.json
+../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator generate -g typescript-fetch -i ./spec/openapi.yaml -o typescript-fetch-es6 -c config.json
 cp ./typescript-fetch-transpile/* typescript-fetch-es6
 cd typescript-fetch-es6
 npm install
