a73404475ee97b3feed6696692fb86fdbcb09eed
Kjetil Mehl <kjetil@mehl.no>
2019-08-06 21:26:20 +0200

    Moves typescript client into subfolder

--- generate-typescript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# TODO: fix
-../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator generate -g typescript-fetch -i ./spec/openapi.yaml -o typescript-fetch-es6 -c config.json
-cp ./typescript-fetch-transpile/* typescript-fetch-es6
-cd typescript-fetch-es6
-npm install
-npm run build
